aafd77c97d4931d43ff3b7e17bcf6201fc99e20a
ik <rechad.kheerdali@gmail.com>
2021-05-10 21:13:07 +0100

    removed others directory

--- other/2018-stephenStreamsProject-CRUD-RESTConventions-googleOauth-Modal/client/src/history.js
+++
@@ -1,5 +0,0 @@
-/*iK created our own custom history object in <Router> instead of using the <BrowserRouter> history object. */
-
-import createHistory from 'history/createBrowserHistory';
-
-export default createHistory();
